771742caaee96b0e6a68d25e1b708acfdc784c51
Austin Kenny <austinkennywork@gmail.com>
2024-04-03 12:43:25 -0400

    added adult test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -45,4 +45,27 @@ RSpec.describe Vehicle do
 
         expect(vehicle.passengers).to eq([])
     end
+
+    it "adds passengers to the vehicle" do
+        vehicle = Vehicle.new("2001", "Honda", "Civic")
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+        jude = Passenger.new({"name" => "Jude", "age" => 20})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+        vehicle.add_passenger(charlie)
+        vehicle.add_passenger(jude)
+        vehicle.add_passenger(taylor)
+
+        expect(vehicle.passengers).to eq([charlie, jude, taylor])
+        #Had some issues figuring out how to write the expect statement for this
+    end
+
+    it "knows how many adults are in the vehicle" do
+        vehicle = Vehicle.new("2001", "Honda", "Civic")
+        charlie = Passenger.new({"name" => "Charlie", "age" => 18}) 
+        jude = Passenger.new({"name" => "Jude", "age" => 20})
+        taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+
+        
+    end
 end
