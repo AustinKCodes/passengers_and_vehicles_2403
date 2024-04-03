4e428cb722ce58324b34f2d031da825deecf7431
Austin Kenny <austinkennywork@gmail.com>
2024-04-03 12:03:29 -0400

    added second test

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,5 +1,5 @@
-require ./lib/vehicle
-require ./lib/passenger
+require './lib/vehicle'
+require './lib/passenger'
 
 RSpec.describe Vehicle do
     it "creates a vehicle" do
@@ -8,4 +8,9 @@ RSpec.describe Vehicle do
         expect(vehicle).to be_instance_of(Vehicle)
     end
 
+    it "returns the year of the vehicle" do
+        vehicle = Vehicle.new("2001", "Honda", "Civic")
+
+        expect(vehicle.year).to eq("2001")
+    end
 end
